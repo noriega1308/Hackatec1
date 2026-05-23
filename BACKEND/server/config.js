f3dffeffd3e2ebad20d6bbd2fb9caff05903450e
noriega1308 <orlandonoriega1308@gmail.com>
2026-05-23 07:35:17 -0600

    Config deploy

--- BACKEND/server/config.js
+++ BACKEND/server/config.js
@@ -1,11 +1,9 @@
-// ─── Servidor ───────────────────────────────────────────────
-export const PORT = 4000;
+export const PORT = process.env.PORT || 4000;
 
-// ─── Base de Datos MySQL ─────────────────────────────────────
 export const DB_CONFIG = {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "roocel_servicios",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
